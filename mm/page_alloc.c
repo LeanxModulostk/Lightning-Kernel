693b1179e293879982525515254692c04e51f9ed
Leanpexd <karim.demons5@gmail.com>
2025-02-22 23:30:53 -0500

    Reverter 7

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4242,9 +4242,6 @@ restart:
 		}
 		wake_all_kswapds(order, gfp_mask, ac);
 	}
-	
-	/* Boost DDR bus when memory is low so allocation latency doesn't get too bad */
-	devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 
 	/*
 	 * The adjusted alloc_flags might result in immediate success, so try
