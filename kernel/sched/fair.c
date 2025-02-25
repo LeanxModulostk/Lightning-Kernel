d68ff21a2f7610cd188f4d491e227c11c08a0c76
Leanpexd <karim.demons5@gmail.com>
2025-02-25 14:46:49 -0500

    sched: fair: fix mismerge after uclamp
    The uclamp patches don't merge 100% into this tree. This might still need revision.
    
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7643,10 +7643,10 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 			task_boost > 0 ||
 #ifdef CONFIG_SCHED_TUNE
 			schedtune_task_boost(p) > 0 ||
-#elif  CONFIG_UCLAMP_TASK
-			uclamp_boosted(p) > 0) &&
+#elif CONFIG_UCLAMP_TASK
+			(uclamp_boosted(p) > 0 &&
 #endif
-			walt_should_kick_upmigrate(p, cpu))
+			walt_should_kick_upmigrate(p, cpu)))
 			return false;
 	} else { /* mid cap cpu */
 		if (task_boost > 1)
@@ -7655,6 +7655,7 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 
 	return task_fits_capacity(p, capacity, cpu);
 }
+
 
 static inline bool task_demand_fits(struct task_struct *p, int cpu)
 {
@@ -8439,12 +8440,11 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 	int placement_boost = task_boost_policy(p);
 	u64 start_t = 0;
 	int next_cpu = -1, backup_cpu = -1;
+	int start_cpu;
 #ifdef CONFIG_SCHED_TUNE
 	int boosted = (schedtune_task_boost(p) > 0 || per_task_boost(p) > 0);
-	int start_cpu = get_start_cpu(p);
-#elif  CONFIG_UCLAMP_TASK
+#elif CONFIG_UCLAMP_TASK
 	int boosted = (uclamp_boosted(p) > 0);
-	bool prefer_high_cap = uclamp_latency_sensitive(p);
 #endif
 
 	if (start_cpu < 0)
